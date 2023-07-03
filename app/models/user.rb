1b78ca30f9a8d565faf8280465413c888a4fa0a9
Njalale <fnjalale@gmail.com>
2023-07-04 02:12:15 +0300

    Added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
     validates :role, presence: true
     validates :phone, presence: true, length: {is: 10}
     validates :password, length: {in: 6..15}
