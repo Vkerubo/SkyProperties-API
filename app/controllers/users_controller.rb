7ccdfa683aae87e969c452c157f7d4879c68163e
Njalale <fnjalale@gmail.com>
2023-07-04 02:49:35 +0300

    Added skip authorize for create in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ update destroy ]
+  skip_before_action :authorize, only: [:create]
   wrap_parameters format: []
   # GET /users
   def index
