843f21459d7586a8681ad1f8e114e0385ca896f3
Valentine <kerubovalentine1@gmail.com>
2023-07-03 13:57:04 +0300

    feat: scaffold Seller

---
+++ db/migrate/20230703105553_create_sellers.rb
@@ -0,0 +1,11 @@
+class CreateSellers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :sellers do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
