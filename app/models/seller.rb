edcc239a5363c6cc22a5b6bc8541124f8dc31fb8
Njalale <fnjalale@gmail.com>
2023-07-04 00:31:09 +0300

    Associations and validations added

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,2 +1,6 @@
 class Seller < ApplicationRecord
+    validates :name, presence: true
+    validates  :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP}
+    validates  :phone, presence: true, length: {is: 10}
+    has_many :properties
 end
