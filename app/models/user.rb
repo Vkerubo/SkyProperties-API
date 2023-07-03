1939e644a8c563b165cf47bddebb92fd148ea420
Njalale <fnjalale@gmail.com>
2023-07-04 01:35:09 +0300

    Done user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
     belongs_to :seller
     has_secure_password
 
+    validates :username, presence: true, uniqueness: true
+    validates :role, presence: true
+    validates :phone, presence: true, length: {is: 10}
+    validates :password, length: {in: 6..15}
+    validates :buyer_id, uniqueness: true, allow_nil: true
+    validates :seller_id, uniqueness: true, allow_nil: true
 end
