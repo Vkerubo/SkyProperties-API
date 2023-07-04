91acc275d0ab142856b0e409875202ada8da1974
Valentine <kerubovalentine1@gmail.com>
2023-07-04 14:13:38 +0300

    dependent destroy

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,6 +3,6 @@ class Property < ApplicationRecord
   validates :title, :address, :price, :image, :bedrooms, :bathrooms, presence: true
 
   belongs_to :seller
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :buyers, through: :favorites
 end
