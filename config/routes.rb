a0ce45a1fcfd6c438bc184143bfb0a0a61cb3e37
Valentine <kerubovalentine1@gmail.com>
2023-07-04 10:13:00 +0300

    property routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :favorites, only: [:index, :create, :destroy]
-  resources :properties
+  resources :properties, only: [:index, :create, :show, :update, :destroy]
   resources :buyers
   resources :sellers, except: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
