f23bb8d960be0bc9ddd9ccac4541b3beb4d947b3
Njalale <fnjalale@gmail.com>
2023-07-04 11:38:42 +0300

    feat: Added login & logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
   resources :properties, only: [:index, :create, :show, :update, :destroy]
   resources :buyers
   resources :sellers, except: [:index]
+
+  get "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
