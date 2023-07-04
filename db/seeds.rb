c1e4a9a17cfb3cf39a8455cab941715393497d23
Valentine <kerubovalentine1@gmail.com>
2023-07-04 13:29:51 +0300

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,59 @@ require 'faker'
 
 puts "start seeding"
 
-# Generate 100 sample properties
-100.times do
-  # Generate random data for each property
-  title = Faker::Address.street_name
-  address = Faker::Address.full_address
-  price = Faker::Number.number(digits: 6)
-  images = [Faker::LoremFlickr.image, Faker::LoremFlickr.image]
-  bedrooms = Faker::Number.between(from: 1, to: 5)
-  bathrooms = Faker::Number.between(from: 1, to: 4)
-  seller_id = Faker::Number.between(from: 1, to: 10)
+Favorite.delete_all
+Buyer.delete_all
+Property.delete_all
+Seller.delete_all
 
-  # Print the generated data for each property
-  puts "Title: #{title}"
-  puts "Address: #{address}"
-  puts "Price: #{price}"
-  puts "Images: #{images}"
-  puts "Bedrooms: #{bedrooms}"
-  puts "Bathrooms: #{bathrooms}"
-  puts "Seller ID: #{seller_id}"
+
+# Clear existing data
+Favorite.delete_all
+Buyer.delete_all
+Property.delete_all
+Seller.delete_all
+
+# Create sellers
+20.times do
+  name = Faker::Name.name
+  email_name = name.downcase.gsub(' ', '')
+  Seller.create!(
+    name: name,
+    email: "#{email_name}@gmail.com",
+    phone: "07#{Faker::Number.unique.number(digits: 8)}"
+  )
+end
+
+# Create properties
+80.times do
+  Property.create!(
+    title: Faker::Lorem.sentence,
+    address: Faker::Address.full_address,
+    price: Faker::Number.between(from: 100000, to: 1000000),
+    image: Faker::LoremFlickr.image(size: "300x200"),
+    bedrooms: Faker::Number.between(from: 1, to: 5),
+    bathrooms: Faker::Number.between(from: 1, to: 3),
+    seller_id: Seller.pluck(:id).sample
+  )
+end
+
+# Create buyers
+20.times do
+  name = Faker::Name.name
+  email_name = name.downcase.gsub(' ', '')
+  Buyer.create!(
+    name: name,
+    email: "#{email_name}@gmail.com",
+    phone: "07#{Faker::Number.unique.number(digits: 8)}"
+  )
+end
+
+# Create favorites
+100.times do
+  Favorite.create!(
+    buyer_id: Buyer.pluck(:id).sample,
+    property_id: Property.pluck(:id).sample
+  )
 end
 
 puts "done seeding"
